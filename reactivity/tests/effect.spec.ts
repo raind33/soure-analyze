e29643d824c2ebf0b1060cb6c3c731a7cd15041b
wuyl <2331336537@qq.com>
2022-05-18 23:24:41 +0800

    feat: 添加reactivity单测

---
+++ reactivity/tests/effect.spec.ts
@@ -0,0 +1,16 @@
+describe('effect', () => {
+  test('normal', () => {
+    const user = reactive({
+      age: 2
+    })
+    let nextAge
+    effect(() => {
+      nextAge = user.age + 1
+    })
+    expect(nextAge).toBe(3)
+    user.age = 5
+    expect(nextAge).toBe(6)
+  })
+})
+
+export {}
---
+++ reactivity/tests/reactive.spec.ts
@@ -0,0 +1,9 @@
+describe('reactive', () => {
+  test('normal', () => { 
+    const target = { num: 1 }
+    const observed = reactive(target)
+
+    expect(target).not.toBe(observed)
+    expect(observed.num).toBe(1)
+   })
+})
