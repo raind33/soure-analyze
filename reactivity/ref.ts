b58253a7b8ce03f5ce6ef3eca0d72d18f1793052
wuyl <2331336537@qq.com>
2022-05-22 10:39:11 +0800

    feat: unref, isref, proxyRefs实现

--- reactivity/ref.ts
+++ reactivity/ref.ts
@@ -1,3 +1,4 @@
+import { unref } from "vue"
 import { isObject } from "../shared/utils"
 import { isTracking, trackEffects, triggerEffects } from "./effect"
 import { reactive } from "./reactive"
@@ -6,6 +7,7 @@ import { Dep } from "./types"
 class RefImpl<T> {
   private _value?: any
   deps: Dep = new Set()
+  __v_isRef: boolean = true
   private _rawValue: any
   constructor(value:any) {
     this._value = convert(value)
@@ -36,4 +38,27 @@ function hasChanged(newVal:any, oldVal:any) {
 }
 export function ref<T>(val?: T)  {
   return new RefImpl<T>(val)
+}
+
+export function isRef<T>(ref: any): ref is RefImpl<T> {
+  return !!(ref as RefImpl<any>).__v_isRef
+}
+
+export function unRef(ref: any) {
+  return isRef(ref) ? ref.value : ref
+}
+
+export function proxyRefs(objectWithRef: any) {
+  return new Proxy(objectWithRef, {
+    get(target, key) {
+      return unref(target[key])
+    },
+    set(target, key, val) {
+      if(isRef(target[key]) && !isRef(val)) {
+        return target[key].value = val
+      } else {
+        return Reflect.set(target, key, val )
+      }
+    }
+  })
 }
--- reactivity/tests/ref.spec.ts
+++ reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect"
-import { ref } from "../ref"
+import { reactive } from "../reactive"
+import { isRef, proxyRefs, ref, unRef } from "../ref"
 
 describe('ref', () => {
   test('normal', () => { 
@@ -54,5 +55,38 @@ describe('ref', () => {
     expect(b).toBe(1)
     a.value = obj
     expect(calls).toBe(1)
+  })
+
+  test('isRef', () => {
+    const a = ref(1)
+    const b = reactive({
+      c: 99
+    })
+    expect(isRef(a)).toBe(true)
+    expect(isRef(b)).toBe(false)
+  })
+  test('unRef', () => {
+    const a = ref(1)
+    expect(unRef(a)).toBe(1)
+    expect(unRef(1)).toBe(1)
+  })
+
+  test('proxyRefs', () => {
+    const user = {
+      age: ref(1),
+      name: 'rain'
+    }
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(1)
+    expect(proxyUser.age).toBe(1)
+    expect(proxyUser.name).toBe('rain')
+
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
+
+    proxyUser.age = ref(30)
+    expect(proxyUser.age).toBe(30)
+    expect(user.age.value).toBe(30)
   })
 })
