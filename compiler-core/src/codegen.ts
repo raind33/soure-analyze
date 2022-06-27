9026fc40d0ad65fc67e3e6ed6af8bd6f5a3d86a2
wuyl <2331336537@qq.com>
2022-06-27 22:58:06 +0800

    feat: 代码优化

--- compiler-core/src/codegen.ts
+++ compiler-core/src/codegen.ts
@@ -1,11 +1,17 @@
+
 export function generate(ast:any) {
   const context = createCodegenContext()
+  const { push } = context
   const args = ['_ctx', '_cache', '$props', '$setup', '$data', '$options']
   const functionName = 'render'
-  const renderContent = `return "rain"`
-  context.push(`function ${functionName}(${args.join(',')}) {`)
-  context.push(renderContent)
-  return context.code
+  const node = ast.codegenNode
+  const renderContent = `return "${node.content}"`
+  push('return ')
+  push(`function ${functionName}(${args.join(',')}) {`)
+  push(renderContent)
+  return {
+    code: context.code
+  }
 }
 
 function createCodegenContext() {
